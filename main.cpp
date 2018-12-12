e40e1e6fb87422ddac68c693756e1642d899b930
Rivka Schuss <rschuss123@gmail.com>
2018-12-12 19:37:24 +0200

    fhhfhffh

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "test" << std::endl;
     std::cout << "boo" << std::endl;
+    std::cout << "hiyoosh" << std::endl;
+    
     return 0;
 }
